3cf86f33014c79a6bb16941c1aa3dac746248ee4
Amanda Pollard <Amandappollard@gmail.com>
2025-01-14 16:31:03 +0000

    feat: got symbol working, moving on to name

--- element.sh
+++ element.sh
@@ -43,21 +43,23 @@ GET_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol='$ELEMENT_ENTERED'"
       then
         echo "I could not find that element in the database."
       else
-      #echo "The element with atomic number $ELEMENT_ENTERED is $GET_NAME($GET_SYMBOL). It's a $GET_TYPE, with a mass of $GET_MASS amu. $GET_NAME has a melting point of $GET_MELT celsius and a boiling point of $GET_BOIL celsius."
+      #echo "The element with atomic number $SYM_NUM is $SYM_NAME($GET_SYMBOL). It's a $SYM_TYPE, with a mass of $SYM_MASS amu. $SYM_NAME has a melting point of $SYM_MELT celsius and a boiling point of $SYM_BOIL celsius."
       
-SYM_SYMBOL=$($PSQL "SELECT symbol FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.ref='$SYM_NUM'")
+#use $GET_SYMBOL as symbol
 
-SYM_NAME=$($PSQL "SELECT name FROM elements FULL JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.ref='$SYM_NUM'")
+SYM_NUM=$($PSQL "SELECT ref FROM elements WHERE symbol='$GET_SYMBOL'")
+
+SYM_NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$GET_SYMBOL'")
 
 SYM_MASS=$($PSQL "SELECT atomic_mass FROM properties FULL JOIN elements ON properties.atomic_number = elements.atomic_number WHERE properties.ref='$SYM_NUM'")
 
 SYM_MELT=$($PSQL "SELECT melting_point_celsius FROM properties FULL JOIN elements ON properties.atomic_number = elements.atomic_number WHERE properties.ref='$SYM_NUM'")
 
-SYM_BOIL=$($PSQL "SELECT melting_point_celsius FROM properties FULL JOIN elements ON properties.atomic_number = elements.atomic_number WHERE properties.ref='$SYM_NUM'")
+SYM_BOIL=$($PSQL "SELECT melting_point_celsius FROM properties FULL JOIN elements ON properties.atomic_number=elements.atomic_number WHERE properties.ref='$SYM_NUM'")
 
 SYM_TYPE=$($PSQL "SELECT type FROM types FULL JOIN properties ON types.type_id = properties.type_id WHERE properties.ref='$SYM_NUM'")
 
-      echo "got sym" $ELEMENT_ENTERED
+  echo "The element with atomic number $SYM_NUM is $SYM_NAME($GET_SYMBOL). It's a $SYM_TYPE, with a mass of $SYM_MASS amu. $SYM_NAME has a melting point of $SYM_MELT celsius and a boiling point of $SYM_BOIL celsius."
       fi
   else 
   #echo "The element with atomic number $ELEMENT_ENTERED is $GET_NAME($GET_SYMBOL). It's a $GET_TYPE, with a mass of $GET_MASS amu. $GET_NAME has a melting point of $GET_MELT celsius and a boiling point of $GET_BOIL celsius."
